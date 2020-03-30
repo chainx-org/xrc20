b00752c25f612ddbec605cae0931978faf7ad686
qinghuan <hashlock@163.com>
2020-03-30 15:52:06 +0800

    update deploy script

--- scripts/dev-deploy-btcxrc20.js
+++ scripts/dev-deploy-btcxrc20.js
@@ -17,18 +17,31 @@
 var program = require('commander');
 var fs = require('fs');
 const Chainx = require('chainx.js').default;
-const {compactAddLength} = require('@chainx/util');
-const {Abi} = require('@chainx/api-contract');
+const { compactAddLength } = require('@chainx/util');
+const { Abi } = require('@chainx/api-contract');
 
 program
     .version('0.1.0')
     .option('-s, --set ', 'Set BTC Xrc20 Address')
+    .option('-y, --deploy', 'sigal deploy contract')
     .option('-d, --deposit [VALUE]', 'Deposit BTC to Address')
     .option('-c, --claim ', 'Claim PCX')
-    .option('-w, --wasm [WASM-PATH]', 'Path of the compiled wasm file', '../target/xrc20.wasm')
-    .option('-a, --abi [ABI-PATH]', 'Path of the generated ABI file', '../target/abi.json')
+    .option('-w, --wasm [WASM-PATH]', 'Path of the compiled wasm file', '/Users/qinghuan/Downloads/xrc20/target/xrc20.wasm')
+    .option('-a, --abi [ABI-PATH]', 'Path of the generated ABI file', '/Users/qinghuan/Downloads/xrc20/target/metadata.json')
     .option('-W, --ws [WEBSOCKET]', 'Webscoket of the ChainX node', 'ws://127.0.0.1:8087')
     .parse(process.argv);
+
+const parseParams = (args, params) => {
+    args.forEach((arg, i) => {
+        const t = arg.type.type
+        if (t.startsWith('u')) {
+            params[i] = parseInt(params[i])
+        } else if (t === 'bool' && typeof params[i] === 'string') {
+            params[i] = JSON.parse(params[i].toLowerCase())
+        }
+    })
+    return params
+}
 
 (async function () {
     var wasmPath = program.wasm
@@ -44,49 +57,61 @@ program
     let xrc20 = new Abi(abi)
     // node websocket ip and port
     const chainx = new Chainx(websocket);
-    let gasLimit= 20000000;
+    let gasLimit = 20000000;
 
     await chainx.isRpcReady();
     if (program.set) {
         let putcode = chainx.api.tx.xContracts.putCode(gasLimit, compactAddLength(wasm))
         putcode.signAndSend(alicePrikey, (error, response) => {
-            if (error){
+            if (error) {
                 console.log(error.message)
                 process.exit(0);
             }
+            console.log(response)
+
             for (var i = 0; response && response.events && (i < response.events.length); i++) {
                 if ('CodeStored' == response.events[i].method) {
-                    let code = response.events[i].event.data[0]
-                    console.log("upload and set wasm code success...", code)
-                    //实例化合约
-                    let instantiate = chainx.api.tx.xContracts.instantiate(1, gasLimit, code, xrc20.constructors[0](0, 'ChainX XRC20-Bitcoin', 'XRC20-BTC', 8))
-                    instantiate.signAndSend(alicePrikey, (error, response) => {
-                        if (error){
+                    let codeHash = response.events[i].event.data[0]
+                    console.log("upload and set wasm code success...", codeHash)
+
+                    const method = 'instantiate'
+                    params = [0, 'ChainX XRC20-Bitcoin', 'XRC20-BTC-6', 8]
+                    parseParams(xrc20.constructors[0].args, params)
+                    const selector = JSON.parse(abi.contract.constructors[0].selector)
+                    const args = [
+                        0,
+                        gasLimit,
+                        codeHash,
+                        selector.reduce((a, b) => a + b.slice(2)) +
+                        xrc20.constructors[0](...params).slice(2)
+                    ]
+                    const ex = chainx.api.tx.xContracts[method](...args)
+                    ex.signAndSend(alicePrikey, (error, response) => {
+                        if (error) {
                             console.log(error.message)
                             process.exit(0);
                         }
-
                         for (var i = 0; response && response.events && (i < response.events.length); i++) {
-                            if ('Instantiated' == response.events[i].method) {
-                                let contract_address = response.events[i].event.data[1]
+                            if ('ContractExecution' == response.events[i].method) {
+                                let contract_address = response.events[i].event.data[0]
                                 console.log("instance xrc20 contract... addr:", contract_address)
 
                                 // notice the selector must match abi.json(or old_abi.json), hex format
                                 let execute = chainx.trustee.execute('0x67df26a755e0c31ac81e2ed530d147d7f2b9a3f5a570619048c562b1ed00dfdd',
                                     chainx.api.tx.xContracts.setTokenXrc20('BTC', contract_address,
                                         new Map([
-                                            ['BalanceOf', '0xe41dbb26'],
-                                            ['TotalSupply', '0x38935d92'],
-                                            ['Name', '0xd0411301'],
-                                            ['Symbol', '0x54e93e98'],
-                                            ['Decimals', '0xe0446593'],
-                                            ['Issue', '0x9d64838c'],
-                                            ['Destroy', '0x18c9374b']
+                                            ['BalanceOf', '0x26BB1DE4'],
+                                            ['TotalSupply', '0x925D9338'],
+                                            ['Name', '0x011341D0'],
+                                            ['Symbol', '0x983EE954'],
+                                            ['Decimals', '0x936544E0'],
+                                            ['Issue', '0x8C83649D'],
+                                            ['Destroy', '0x4B37C918']
                                         ])
                                     )
                                 );
                                 execute.signAndSend(alicePrikey, (error, response) => {
-                                    if (error){
+                                    if (error) {
                                         console.log(error.message)
                                         process.exit(0);
                                     }
@@ -94,21 +119,21 @@ program
                                         if ('ExtrinsicSuccess' == response.events[i].method) {
                                             console.log('set xrc20 contract addr success...')
                                             process.exit(0);
-                                        }else if ('ExtrinsicFailed' == response.events[i].method) {
+                                        } else if ('ExtrinsicFailed' == response.events[i].method) {
                                             console.log('set erc20 contract addr fail...')
                                             process.exit(0);
                                         }
                                     }
                                 })
-                                break
-                            }else if ('ExtrinsicFailed' == response.events[i].method) {
-                                console.log("instance erc20 contract fail... ")
+                                // breakc
+                            } else if ('ExtrinsicFailed' == response.events[i].method) {
+                                console.log("instance erc20 contract fail ")
                                 process.exit(0);
                             }
                         }
                     })
                 } else if ('ExtrinsicFailed' == response.events[i].method) {
-                    console.log("upload and set wasm code fail...",)
+                    console.log("instance erc20 contract fail... ")
                     process.exit(0);
                 }
             }
@@ -119,7 +144,7 @@ program
             chainx.api.tx.xAssetsRecords.depositFromRoot(program.deposit, 'BTC', 100000000)
         );
         execute.signAndSend(alicePrikey, (error, response) => {
-            if (error){
+            if (error) {
                 console.log(error.message)
                 process.exit(0);
             }
@@ -127,19 +152,82 @@ program
                 if ('ExtrinsicSuccess' == response.events[i].method) {
                     console.log('Deposit ChainX X-BTC success...')
                     process.exit(0);
-                }else if ('ExtrinsicFailed' == response.events[i].method) {
-                    console.log("Deposit Erc20 BTC fail...",)
+                } else if ('ExtrinsicFailed' == response.events[i].method) {
+                    console.log("Deposit Erc20 BTC fail...")
                     process.exit(0);
                 }
             }
         })
+    } else if (program.deploy) {
+        //code hash, only deploy
+        let codeHash = '0xd71d75d5cb24bddfa7d06c638f1b48ad43f8819ce927a714bed0db985dcf5348'
+
+        const method = 'instantiate'
+        params = [0, 'ChainX XRC20-Bitcoin', 'XRC20-BTC-7', 8]
+        parseParams(xrc20.constructors[0].args, params)
+        const selector = JSON.parse(abi.contract.constructors[0].selector)
+        const args = [
+            0,
+            gasLimit,
+            codeHash,
+            selector.reduce((a, b) => a + b.slice(2)) +
+            xrc20.constructors[0](...params).slice(2)
+        ]
+        const ex = chainx.api.tx.xContracts[method](...args)
+        ex.signAndSend(alicePrikey, (error, response) => {
+            if (error) {
+                console.log(error.message)
+                process.exit(0);
+            }
+            for (var i = 0; response && response.events && (i < response.events.length); i++) {
+                if ('ContractExecution' == response.events[i].method) {
+                    let contract_address = response.events[i].event.data[0]
+                    console.log("instance xrc20 contract... addr:", contract_address)
+
+                    // notice the selector must match abi.json(or old_abi.json), hex format
+                    let execute = chainx.trustee.execute('0x67df26a755e0c31ac81e2ed530d147d7f2b9a3f5a570619048c562b1ed00dfdd',
+                        chainx.api.tx.xContracts.setTokenXrc20('BTC', contract_address,
+                            new Map([
+                                ['BalanceOf', '0x26BB1DE4'],
+                                ['TotalSupply', '0x925D9338'],
+                                ['Name', '0x011341D0'],
+                                ['Symbol', '0x983EE954'],
+                                ['Decimals', '0x936544E0'],
+                                ['Issue', '0x8C83649D'],
+                                ['Destroy', '0x4B37C918']
+                            ])
+                        )
+                    );
+                    execute.signAndSend(alicePrikey, (error, response) => {
+                        if (error) {
+                            console.log(error.message)
+                            process.exit(0);
+                        }
+                        for (var i = 0; response && response.events && (i < response.events.length); i++) {
+                            if ('ExtrinsicSuccess' == response.events[i].method) {
+                                console.log('set xrc20 contract addr success...')
+                                process.exit(0);
+                            } else if ('ExtrinsicFailed' == response.events[i].method) {
+                                console.log('set erc20 contract addr fail...')
+                                process.exit(0);
+                            }
+                        }
+                    })
+                    // breakc
+                } else if ('ExtrinsicFailed' == response.events[i].method) {
+                    console.log("instance erc20 contract fail ")
+                    process.exit(0);
+                }
+            }
+        })
+
     } else if (program.xrc) {
         let execute = chainx.api.tx.xContracts.convertToXrc20('BTC',
             10000,
             50000000
         );
         execute.signAndSend(alicePrikey, (error, response) => {
-            if (error){
+            if (error) {
                 console.log(error.message)
                 process.exit(0);
             }
@@ -147,16 +235,16 @@ program
                 if ('ExtrinsicSuccess' == response.events[i].method) {
                     console.log('Convert Xrc20-BTC success...')
                     process.exit(0);
-                }else if ('ExtrinsicFailed' == response.events[i].method) {
-                    console.log("Convert Xrc20-BTC fail...",)
+                } else if ('ExtrinsicFailed' == response.events[i].method) {
+                    console.log("Convert Xrc20-BTC fail...")
                     process.exit(0);
                 }
             }
         })
-    }else if (program.claim){
+    } else if (program.claim) {
         let execute = chainx.stake.voteClaim('0x88dc3417d5058ec4b4503e0c12ea1a0a89be200fe98922423d4334014fa6b0ee');
         execute.signAndSend(alicePrikey, (error, response) => {
-            if (error){
+            if (error) {
                 console.log(error.message)
                 process.exit(0);
             }
@@ -164,8 +252,8 @@ program
                 if ('ExtrinsicSuccess' == response.events[i].method) {
                     console.log('Claim PCX success...')
                     process.exit(0);
-                }else if ('ExtrinsicFailed' == response.events[i].method) {
-                    console.log("Claim PCX fail...",)
+                } else if ('ExtrinsicFailed' == response.events[i].method) {
+                    console.log("Claim PCX fail...")
                     process.exit(0);
                 }
             }
@@ -173,7 +261,7 @@ program
     }
     else {
         console.log("Error Cmd...")
-	    process.exit(0);
+        process.exit(0);
     }
 
 })()
